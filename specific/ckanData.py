8a1a54d6b7cd7efcd8085ef0dea376eb4b355a30
Carlos <carlos.horro@gmail.com>
2015-09-08 09:33:56 +0100

    Lots of bug fixes, updating and full updating policy support, added source and insertion date fields

--- specific/ckanData.py
+++ specific/ckanData.py
@@ -1,7 +1,7 @@
 import json
 import requests
-import pysolr
 import sys
+from datetime import datetime, timedelta, date, time
 
 # Importing db manager
 sys.path.insert(0, '../../resource-contextualization-import-db/abstraction')
@@ -75,20 +75,17 @@ def get_title(data):
         * {string} Return 'title' value from the list. None if there is any error.
     """
     
-    get_one_field_from_tm_data(data, 'title')
+    return get_one_field_from_tm_data(data, 'title')
     
-
 
 def get_notes(data):
     """
-        Get 'notes' (description) field from the data of one training material.
+        Get 'notes' field from the data of one training material.
         * data {list} data of one training material.
         * {string} Return 'notes' value from the list. None if there is any error.
     """
     
-    get_one_field_from_tm_data(data, 'notes')
-
-
+    return get_one_field_from_tm_data(data, 'notes')
 
 
 def get_field(data):
@@ -97,28 +94,124 @@ def get_field(data):
         * data {list} data of one training material.
         * {string} Return 'field' value from the list. None if there is any error.
     """
+    return get_ckan_data_type()
 
+
+def get_ckan_data_type():
+    """
+        Get specific data type of fields related with CKAN.
+        * {string} Return data type of ckan fields.
+    """
     return 'Training Materials'
+
+
+def get_source_type_field():
+    """
+        Get source type of any registry obtained with this script.
+        * {string} Return source type value.
+    """
+    return get_ckan_source_type()
+
+
+def get_ckan_source_type():
+    """
+        Get specific data type of fields related with CKAN.
+        * {string} Return data type of ckan fields.
+    """
+    return 'ckan'
+
+
+def get_insertion_date_field():
+    """
+        Get insertion date of any registry obtained with this script.
+        * {date} Return source type value.
+    """
+    return datetime.now()
+
+
+def get_created(data):
+    """
+        Get 'created' field from the data of one training material.
+        * data {list} data of one training material.
+        * {string} Return 'created' value from the list. None if there is any error.
+    """   
+    return get_one_field_from_tm_data(data, 'metadata_created')
+
+
+def isDataMoreRecentThan(data, minimumDate):
+    """
+        Returns if data passed as argument is more recient than the minimumDate argument.
+        * data {list} data of one training material.
+        * minimumDate {datetime} minimum date and time that registry should be. 
+        * {string} Return 'created' value from the list. None if there is any error.
+    """  
+    if minimumDate is not None:
+        createdDateString = get_created(data)
+        if createdDateString is not None:
+            datetime_object = datetime.strptime(createdDateString, '%Y-%m-%dT%H:%M:%S.%f' )
+            return (minimumDate < datetime_object)
+        else:
+            return False
+    else:
+        return True
+    
+
+
+
+###    ENTRY POINTS
+
 
 
 
 def main():
+    """
+        Executes main_options function with default configurations
+    """
     main_options(None)
+
+       
+def mainUpdating(registriesFromTime):
+    """
+        Executes main_options function with a time from wich to add new registries.
+        registriesFromTime {datetime} time from registries will be obtained
+    """
+    my_options = {}
+    my_options['registriesFromTime'] = registriesFromTime
+    main_options(my_options)
+    
+       
+def mainFullUpdating():
+    """
+        Executes main_options function erasing all previous ckan data
+    """
+    my_options = {}
+    my_options['delete_all_old_data'] = True
+    main_options(my_options)
+    
     
 def main_options(options):
     """
         Executes the main functionality of this script: it extracts JSON data from each Training Material found
         and inserts its main data into the DB.
         * options {list} specific configurations for initialization.
-            ds_name: specific dataset/database to use with the DB manager
+            ds_name {string} specific dataset/database to use with the DB manager
+            delete_all_old_data {boolean} specifies if we should delete all previous ckanData in our DataBase
+            registriesFromTime {datetime} time from registries will be obtained
+
         
     """
 
     print ('>> Starting ckanData importing process...')
     
     ds_name = None
+    delete_all_old_data = False
     if options is not None:
-        ds_name = options['ds_name']
+        if ('ds_name' in options.keys()):
+            ds_name = options['ds_name']
+        if ('delete_all_old_data' in options.keys()):
+            delete_all_old_data = options['delete_all_old_data']
+        if ('registriesFromTime' in options.keys()):
+            registriesFromTime = options['registriesFromTime']
     
     
     materials_names = get_materials_names()
@@ -127,15 +220,28 @@ def main_options(options):
         # print (dbFactory)
         dbManager = dbFactory.get_default_db_manager(ds_name)
         # print (dbManager)
+        if (delete_all_old_data is not None and delete_all_old_data):
+            dbManager.delete_data_by_conditions([['EQ','source',get_source_type_field()]])
         
         for material_name in materials_names:
             json_data = get_json_from_material_name(material_name)
             if (json_data is not None):
-                dbManager.insert_data({"title":get_title(json_data), "notes":get_notes(json_data), "field":get_field(json_data)})
-                
+                # If we have registriesFromTime, we have to check that each one's creation date if more recent than registriesFromTime
+                if registriesFromTime is None or isDataMoreRecentThan(json_data,registriesFromTime):
+                    print(json_data)
+                    dbManager.insert_data({
+                        "title":get_title(json_data),
+                        "notes":get_notes(json_data),
+                        "field":get_field(json_data),
+                        "source":get_source_type_field(),
+                        "insertion_date":get_insertion_date_field()
+                        })
     print ('< Finished ckanData importing process...')   
 
 
 if __name__ == "__main__":
     # main_options({"ds_name":'test_core'})
-    main()
+    mainFullUpdating()
+    #now = datetime.now()
+    #oneweekbefore = now-(timedelta(days=100))
+    #mainUpdating(oneweekbefore)
--- specific/iannData.py
+++ specific/iannData.py
@@ -1,29 +1,40 @@
 from __future__ import print_function
-import pysolr
 import re
 import sys
+import datetime
+import pysolr
 
 # Importing db manager
 sys.path.insert(0, '../../resource-contextualization-import-db/abstraction')
 from DB_Factory import DBFactory
 
 
-def get_iann_data():
+def get_iann_data(registriesFromTime):
     """
         Makes a Request to the Solr Server from "iann.pro"
+        * registriesFromTime {datetime} time from registries will be obtained
+        
+        Some information about iAnn SolR server:
         * iannData {class} url - Uniform Resource Locator
         * {class} resultsIann Query fields:
            "q='*:*'" - Query all the data;
            "rows='5000'" - Indicates the maximum number of events that will be returned;
            "fq='start:[2015-01-01T00:00:00Z TO *]" - Events beginning in 2015-01-01 until "*" (today)
+                submission_date: Events created from this date
     """
     
+    myfq='start:[2015-01-01T00:00:00Z TO *]'
+    if registriesFromTime is not None:
+        submission_date = 'submission_date:['+str(registriesFromTime.year)+'-'+str(registriesFromTime.month)+'-'+str(registriesFromTime.day)+'T'+str(registriesFromTime.hour)+':'+str(registriesFromTime.minute)+':'+str(registriesFromTime.second)+'Z TO *]'
+        myfq = myfq + ' AND '+submission_date 
+
     try:
         iannData = pysolr.Solr('http://iann.pro/solr/', timeout=20)
-        resultsIann = iannData.search(q='*:*', rows='5000', fq='start:[2015-01-01T00:00:00Z TO *]')
+        resultsIann = iannData.search(q='*:*', rows='5000', fq=myfq)
         return resultsIann
-    except Exception:
+    except Exception as e:
         print ("Exception asking for iAnn data")
+        print (e)
         return None
 
 
@@ -128,6 +139,31 @@ def get_field(data):
         return None
     
 
+def get_source_type_field():
+    """
+        Get source type of any registry obtained with this script.
+        * {string} Return source type value.
+    """
+    return get_iann_source_type()
+
+
+def get_iann_source_type():
+    """
+        Get specific data type of fields related with iAnn.
+        * {string} Return data type of iAnn fields.
+    """
+    return 'iann'
+
+
+def get_insertion_date_field():
+    """
+        Get insertion date of any registry obtained with this script.
+        * {date} Return source type value.
+    """
+    return datetime.datetime.now()
+
+    
+
 def remove_unicode_chars(variable):
     """
         Utility function to remove special Unicode chars.
@@ -153,8 +189,35 @@ def remove_unicode_chars(variable):
         return None
 
 
+
+###    ENTRY POINTS
+
+
 def main():
+    """
+        Executes main_options function with default configurations
+    """
     main_options(None)
+    
+  
+def mainUpdating(registriesFromTime):
+    """
+        Executes main_options function with a time from wich to add new registries.
+        * registriesFromTime {datetime} time from registries will be obtained
+    """
+    my_options = {}
+    my_options['registriesFromTime'] = registriesFromTime
+    main_options(my_options)
+    
+    
+def mainFullUpdating():
+    """
+        Executes main_options function erasing all previous iAnn data
+    """
+    my_options = {}
+    my_options['delete_all_old_data'] = True
+    main_options(my_options)
+         
     
 def main_options(options):
     """
@@ -162,15 +225,21 @@ def main_options(options):
         into the DB
         * options {list} specific configurations for initialization.
             ds_name: specific dataset/database to use with the DB manager
+            delete_all_old_data {boolean} specifies if we should delete all previous ckanData in our DataBase
+            registriesFromTime {datetime} time from registries will be obtained
                
-        variables {string}:
-        "title" - Title for the event;
-        "start" - Date the event starts;
-        "end" - Date the event ends;
-        "city" - City where the event is hosted;
-        "country" - Country where the event is hosted;
-        "provider" - Organization hosting the event;
-        "field" - Branches of science in molecular biology;
+        
+        In this script we will insert these fields into each registry:
+            "title" {string} Title for the event;
+            "start" - Date the event starts;
+            "end" - Date the event ends;
+            "city" {string} City where the event is hosted;
+            "country" {string} Country where the event is hosted;
+            "field" {string} Branches of science in molecular biology.
+            "provider" {string} Organization hosting the event;
+            "link" {string} Link to the data registry.
+            "source" {string} Default ('ckan');
+            "insertion date" {date} Current date and time.
 
         See more eg: http://iann.pro/iann-web-services
     """
@@ -178,26 +247,47 @@ def main_options(options):
     print ('>> Starting iann importing process...')
     
     ds_name = None
+    delete_all_old_data = False
+    registriesFromTime = None
+
     if options is not None:
-        ds_name = options['ds_name']
+        if ('ds_name' in options.keys()):
+            ds_name = options['ds_name']
+        if ('delete_all_old_data' in options.keys()):
+            delete_all_old_data = options['delete_all_old_data']
+        if ('registriesFromTime' in options.keys()):
+            registriesFromTime = options['registriesFromTime']
     
 
-    iann_data = get_iann_data()
-    
+    iann_data = get_iann_data(registriesFromTime)
     if iann_data is not None:
-        dbFactory = DBFactory()
-        # print (dbFactory)
+        dbFactory = DBFactory()       
         dbManager = dbFactory.get_default_db_manager(ds_name)
-        # print (dbManager)
+        
+        if (delete_all_old_data is not None and delete_all_old_data):
+            dbManager.delete_data_by_conditions([['EQ','source',get_source_type_field()]])
+        
         for result in iann_data:
-            if (result is not None):               
-                dbManager.insert_data({"title":get_title(result), "start":get_start(result), "end":get_end(result),
-                              "city":get_city(result), "country":get_country(result), "field":get_field(result),
-                              "provider":get_provider(result), "link":get_link(result)})
-     
+            if (result is not None):
+                dbManager.insert_data({
+                    "title":get_title(result),
+                    "start":get_start(result),
+                    "end":get_end(result),
+                    "city":get_city(result),
+                    "country":get_country(result),
+                    "field":get_field(result),
+                    "provider":get_provider(result),
+                    "link":get_link(result),
+                    "source":get_source_type_field(),
+                    "insertion_date":get_insertion_date_field()
+                    })
+                
     print ('< Finished iann importing process...')   
 
 
 if __name__ == "__main__":
     # main_options({"ds_name":'test_core'})
-    main()
+    # mainFullUpdating()
+    now = datetime.datetime.now()
+    oneweekbefore = now-datetime.timedelta(days=7)
+    mainUpdating(oneweekbefore)
