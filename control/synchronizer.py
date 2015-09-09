765b625e3d9db60c637b31544c75442c4f62a2ad
Carlos <carlos.horro@gmail.com>
2015-09-09 13:01:06 +0100

    synchronizer logging, more parametrizable

--- control/synchronizer.py
+++ control/synchronizer.py
@@ -3,6 +3,8 @@
 #!/usr/bin/env python
 
 # You need to install APScheduler:  https://pypi.python.org/pypi/APScheduler/
+# Full documentation:
+# https://media.readthedocs.org/pdf/apscheduler/latest/apscheduler.pdf
 # sudo python setup.py install
 # See CRON documentation at : http://apscheduler.readthedocs.org/en/latest/modules/triggers/cron.html
 
@@ -28,7 +30,7 @@ import iannData
 import registryData
 
 # Functions to be executed every hour for partial updatings
-UPDATING_1H_PROCESSES = [iannData.mainUpdating, ckanData.mainUpdating]
+UPDATING_1H_PROCESSES = [iannData.mainUpdating,ckanData.mainUpdating]
 def getPastHour():
     return datetime.now()-timedelta(hours=1)
     
@@ -44,7 +46,32 @@ def getPastWeek():
  
 
 
-def execute_specific_scripts(params):
+
+
+
+logger = None
+
+def init_logger():
+    """
+        Function that initialises logging system
+    """
+    global logger
+    logger = logging.getLogger('sync_logs')
+    if (len(logger.handlers) == 0):           # We only create a StreamHandler if there aren't another one
+        streamhandler = logging.StreamHandler()
+        streamhandler.setLevel(logging.INFO)
+        logger.setLevel(logging.INFO)
+        # create formatter
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        streamhandler.setFormatter(formatter)
+        # add formatter to ch
+        logger.addHandler(streamhandler)
+    
+        
+
+
+
+def scripts_async_execution(params):
     """
         Function that executes asynchronously all specific synchronisation scripts passed as params.
         * params:
@@ -63,37 +90,32 @@ def execute_specific_scripts(params):
     
     for process_number in range(0, len(processes)):
         # full call example : pool.apply_async(foo_pool, args = (i, ), callback = log_result)
-        print(processes[process_number])
-        pool.apply_async(processes[process_number],[retrievingTimeFunction()] ) 
+        logger.info('SYNCHRONIZING PROCESS... executing '+str(processes[process_number])+' function')
+        pool.apply_async(processes[process_number],[retrievingTimeFunction()] )
+        
     pool.close()
     pool.join()
  
 
-#def synchronize_job():
-    """
-        Function that is to be executed in a thread by the scheduler
-    """
-#    execute_specific_scripts()
- 
- 
+
 
 def start_blocking_scheduler():
     """
         Executes 'synchronize_job' as a synchronous job every hour.
         Console normal entry point   
     """
+    init_logger()
+    logger.info('SYNCHRONIZING PROCESS... starting blocking scheduler')
     sched = BlockingScheduler()
-    updating_1h_job = sched.add_job(execute_specific_scripts, 'cron', minute="03", args=[[UPDATING_1H_PROCESSES, getPastHour]])
-    full_updating_1d_job = sched.add_job(execute_specific_scripts, 'cron', hour="0", args=[[FULL_UPDATING_1D_PROCESSES, getPastDay]])
-    full_updating_1w_job = sched.add_job(execute_specific_scripts, 'cron', day=6, args=[[FULL_UPDATING_1W_PROCESSES, getPastWeek]])
-
-    #job = sched.add_job(execute_specific_scripts, minute=0)
-
+    updating_1h_job = sched.add_job(scripts_async_execution, 'cron', minute="0", args=[[UPDATING_1H_PROCESSES, getPastHour]])
+    full_updating_1d_job = sched.add_job(scripts_async_execution, 'cron', hour="0", args=[[FULL_UPDATING_1D_PROCESSES, getPastDay]])
+    full_updating_1w_job = sched.add_job(scripts_async_execution, 'cron', day=6, args=[[FULL_UPDATING_1W_PROCESSES, getPastWeek]])
 
     try:
         logging.basicConfig()
         sched.start()
     except (KeyboardInterrupt, SystemExit):
+        logger.info('Stopping blocking scheduler')
         pass
     
 
@@ -102,12 +124,15 @@ def start_background_scheduler():
         Executes 'synchronize_job' as an asynchronous job every hour.
         Alternative non-blocking entry point        
     """
+    init_logger()
+    logger.info('SYNCHRONIZING PROCESS... starting background scheduler')
+
     sched = BackgroundScheduler()
     #job = sched.add_job(synchronize_job,'cron' , id='synchronize_job', minute=0)
 
-    updating_1h_job = sched.add_job(execute_specific_scripts, 'cron', minute="48", args=[[UPDATING_1H_PROCESSES, getPastHour]])
-    full_updating_1d_job = sched.add_job(execute_specific_scripts, 'cron', hour="0", args=[[FULL_UPDATING_1D_PROCESSES, getPastDay]])
-    full_updating_1w_job = sched.add_job(execute_specific_scripts, 'cron', day=6, args=[[FULL_UPDATING_1W_PROCESSES, getPastWeek]])
+    updating_1h_job = sched.add_job(scripts_async_execution, 'cron', minute="0", args=[[UPDATING_1H_PROCESSES, getPastHour]])
+    full_updating_1d_job = sched.add_job(scripts_async_execution, 'cron', hour="0", args=[[FULL_UPDATING_1D_PROCESSES, getPastDay]])
+    full_updating_1w_job = sched.add_job(scripts_async_execution, 'cron', day=6, args=[[FULL_UPDATING_1W_PROCESSES, getPastWeek]])
 
     logging.basicConfig()
     sched.start()
@@ -117,6 +142,7 @@ def start_background_scheduler():
         while True:
             time.sleep(2)
     except (KeyboardInterrupt, SystemExit):
+        logger.info('Stopping background scheduler')
         sched.shutdown()  # Not strictly necessary if daemonic mode is enabled but should be done if possible
  
  
