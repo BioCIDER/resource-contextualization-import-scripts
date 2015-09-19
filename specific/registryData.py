4b9b125eedcb350ef08af8fe40fbc1799f148cb8
Carlos <carlos.horro@gmail.com>
2015-09-19 13:57:48 +0100

    New resource_type field. Field now get values from 'topic' source field.

--- specific/registryData.py
+++ specific/registryData.py
@@ -1,4 +1,5 @@
 import requests
+import re
 import json
 import sys
 import datetime
@@ -110,29 +111,63 @@ def get_link(data):
     
     return get_one_field_from_registry_data(data, 'homepage')
     
-    
+
 def get_field(data):
     """
-        Get 'field' field from the data of one record.
-        * data {list} one Elixir's record.
-        * {string} Return 'field' value from the list. None if there is any error.
+        Get 'field' field from the data of one elixir registry entity.
+        * data {list} one record of Elixir registry data.
+        * {string or list} Return 'field' value from the list. None if there is any error.
     """
     
-    return 'Services Registry'
+    my_field = get_one_field_from_registry_data(data, 'topic')
+    if my_field is not None:
+        my_field_converted = eval(my_field)
+        return_value = []
+        for each_field in my_field_converted:
+            try:
+                term = each_field.get('term')
+                return_value.append(term)
+            except Exception as e:
+                logger.error("Error getting 'term' field of "+my_field+" topic:")
+                logger.error(e)
+        return return_value
+    else:
+        return None
 
 
-def get_source_type_field():
+def get_resource_type_field(data):
     """
-        Get source type of any registry obtained with this script.
-        * {string} Return source type value.
+        Get the resource type of any registry obtained with this script.
+        * data {list} one Elixir's record.
+        * {string} Return resource type value.
     """
-    return get_elixir_registry_source_type()
+    my_field = get_one_field_from_registry_data(data, 'resourceType')
+    if my_field is not None:
+        clear_value = remove_unicode_chars(my_field)
+        if isinstance(clear_value, basestring):
+            return clear_value
+        else:
+            return_value = []
+            for each_value in clear_value: 
+                return_value.append(each_value)
+            return return_value
+    
+    else:
+        return None
 
 
-def get_elixir_registry_source_type():
+def get_source_field():
     """
-        Get specific data type of fields related with Elixir registry.
-        * {string} Return data type of Elixir registry fields.
+        Get the source token of any registry obtained with this script.
+        * {string} Return source token.
+    """
+    return get_elixir_registry_source()
+
+
+def get_elixir_registry_source():
+    """
+        Get the specific source of fields related with Elixir registry.
+        * {string} Returns a representative token of the Elixir registry fields source.
     """
     return 'elixir_registry'
 
@@ -144,6 +179,30 @@ def get_insertion_date_field():
     """
     return datetime.datetime.now()
 
+
+
+def remove_unicode_chars(variable):
+    """
+        Utility function to remove special Unicode chars.
+        * variable {string} string variable with Unicode chars. It can contains more than only one different strings. 
+        * {list} Return the variables without Unicode chars. None if there is any error.
+    """
+    if variable is not None and isinstance(variable, basestring) : 
+        listFull = []
+        strText = variable.replace("[u'", "")
+        strClear = strText.replace("']", "")
+    
+        if re.search("', u'", strClear):
+            array = strClear.split("', u'")
+            for index in array:
+                strEach = index
+                listFull.append(strEach)
+            return listFull
+        else:
+            listFull.append(strClear)
+            return listFull
+    else:
+        return None
 
 
 
@@ -232,7 +291,7 @@ def main_options(options):
     dbManager = dbFactory.get_default_db_manager(ds_name)
     
     if (delete_all_old_data is not None and delete_all_old_data):
-        registry_conditions = [['EQ','source',get_source_type_field()]]
+        registry_conditions = [['EQ','source',get_source_field()]]
         previous_count = dbManager.count_data_by_conditions(registry_conditions)
         dbManager.delete_data_by_conditions(registry_conditions)
         new_count = dbManager.count_data_by_conditions(registry_conditions)
@@ -249,7 +308,8 @@ def main_options(options):
                 "notes":get_description(record),
                 "link":get_link(record),
                 "field":get_field(record),
-                "source":get_source_type_field(),
+                "source":get_source_field(),
+                "resource_type":get_resource_type_field(record),
                 "insertion_date":get_insertion_date_field()
             })
             if success:
